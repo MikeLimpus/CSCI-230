4e1fd30ca1ac13b444ffbbe577936637b900ce68
U-CRAFTON\cntl109a035 <cntl109a035@P3630-109-35.crafton.int>
2020-02-04 14:41:16 -0800

    PSet 1 Problem 2

---
+++ PSets/Postfix.java
@@ -0,0 +1,24 @@
+// Jonathan Limpus 
+
+package psets;
+// Imports
+import java.util.Scanner;
+public class Postfix {
+    // Instance Variables 
+    // Define all operators as special characters 
+    static char add = '+', diff = '-', prod = '*', div = '/';
+    static Scanner in = new Scanner(System.in);
+
+    public static void main(String args[]) {
+        System.out.println("Please enter an equation in postfix notation (i.e. 5 2 3 * + 18 3 / +) with spaces");
+        String equation = in.nextLine();
+        for (int i = 0; i < equation.toCharArray().length(); i++;) {
+            switch
+        }
+
+
+    }
+
+
+
+}
