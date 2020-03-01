9728245a7864ab05cd1e69f73d7d74d6dcdf3eed
Mike Limpus <mikelimpus111@gmail.com>
2020-02-29 19:20:22 -0800

    started GuitarString

--- projects/guitar/GuitarString.java
+++ projects/guitar/GuitarString.java
@@ -1,3 +1,21 @@
+/**
+ * Jonathan Limpus
+ * Project 2 - Guitar
+ * CSCI-230
+ * 
+ * GuitarString - Models a vibrating guitar string 
+ */
+
 public class GuitarString {
-    
+    // Instance Variables 
+    private int samplingRate; 
+
+    /** 
+     * Create a guitar string of the argument frequency, using a sampling rate of 
+     * 44,000 by creating a RingBuffer instance of the desired capacity N
+     * Where N = ceiling((sampling rate 44100) / frequency) 
+     */
+    public GuitarString(Double frequency) {
+        
+    }
 }
