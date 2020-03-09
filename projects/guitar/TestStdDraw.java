c6fc29598d4b4b727b9da348f2ac8b20b84b610c
Mike Limpus <mikelimpus111@gmail.com>
2020-03-09 14:08:35 -0700

    played with StdDraw

--- projects/guitar/TestStdDraw.java
+++ projects/guitar/TestStdDraw.java
@@ -1,11 +1,19 @@
 // An example of the class StdDraw
 
 public class TestStdDraw {
-       public static void main(String[] args) {
-           StdDraw.setPenRadius(0.0005);
-           StdDraw.setPenColor(StdDraw.BLUE);
-           StdDraw.point(0.5, 0.5);
-           StdDraw.setPenColor(StdDraw.MAGENTA);
-           StdDraw.line(0.2, 0.2, 0.8, 0.2);
+    public static void main(String[] args) {
+        Double baseX = 0.0;
+        Double baseY = 0.5;
+        for(;;) {
+
+            //double randomX = Math.random();
+            //double randomY = Math.random();
+            StdDraw.setPenRadius(0.005);
+            StdDraw.setPenColor(StdDraw.BLUE);
+            StdDraw.point(baseX, baseY);
+            baseX += 0.0001;
+        }
+           //StdDraw.setPenColor(StdDraw.MAGENTA);
+           //StdDraw.line(0.2, 0.2, 0.8, 0.2);
        }
    }
